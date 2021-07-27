ef81105e663113ffe66f054345a0148f56769595
Vitali <vitali.kuchynski@gmail.com>
2021-07-27 10:50:58 -0400

    schema_update

--- src/main/java/com/otus/model/Staff.java
+++ src/main/java/com/otus/model/Staff.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Staff {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -21,11 +21,7 @@ create table roles (
      name varchar(100) not null
 );
 
-insert into roles values (1, 'admin');
-insert into roles values (2, 'teacher');
-
-CREATE SEQUENCE hibernate_sequence START 3;
-
+insert into roles values (1, 'ADMIN');
 
 create table staff (
     id bigserial primary key,
@@ -34,13 +30,15 @@ create table staff (
     address varchar (100) not null,
     phone varchar (100) not null
 );
-
 insert into staff values (1, 'Valera_Admin', 'vradmin@gmail.com', 'Big street', '2656412452' );
+
+CREATE SEQUENCE hibernate_sequence START 2;
 
 create table staff_roles (
     staff_id bigint,
     roles_id bigint
 );
+insert into staff_roles values (1 , 1);
 
 create table payments (
     id bigserial not null,

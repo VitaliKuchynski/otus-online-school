e845154faf16c7570915e64858ce42935d8a7085
Vitali <vitali.kuchynski@gmail.com>
2021-07-28 10:45:04 -0400

    schema update

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,4 +1,4 @@
-create table if not exists students (
+create table if not exists user (
     id bigserial primary key,
     name varchar(100) not null,
     email varchar (100) not null,
@@ -6,24 +6,24 @@ create table if not exists students (
     phone varchar (100) not null
 );
 
-create table courses (
+create table if not exists courses (
     id bigserial primary key,
     name varchar(100) not null
 );
 
-create table students_courses (
+create table if not exists students_courses (
     course_id bigint,
     student_id bigint
 );
 
-create table roles (
+create table if not exists roles (
      id bigserial primary key,
      name varchar(100) not null
 );
 
 insert into roles values (1, 'ADMIN');
 
-create table staff (
+create table if not exists staff (
     id bigserial primary key,
     name varchar(100) not null,
     email varchar (100) not null,
@@ -31,17 +31,17 @@ create table staff (
     phone varchar (100) not null,
     password varchar (100) not null
 );
-insert into staff values (1, 'Valera_Admin', 'vradmin@gmail.com', 'Big street', '2656412452', '123' );
+insert into staff values (1, 'Valera_Admin', 'vradmin@gmail.com', 'Big street', '2656412452', '123');
 
 CREATE SEQUENCE hibernate_sequence START 2;
 
-create table staff_roles (
+create table if not exists staff_roles (
     staff_id bigint,
     roles_id bigint
 );
 insert into staff_roles values (1 , 1);
 
-create table payments (
+create table if not exists payments (
     id bigserial not null,
     local_date_time timestamp not null default current_timestamp
 );
